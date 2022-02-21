7197d1e686a57f6a8355f131c3f5dc89b88020b8
omid <o.beygi@gmail.com>
2022-02-21 12:56:21 +0330

    2/21

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni omnis ad
+        explicabo quisquam odit voluptas quibusdam, aliquam quos necessitatibus
+        laudantium asperiores tempore soluta, molestiae deleniti? Animi tenetur
+        sed nulla soluta mollitia, similique dolor tempore fuga temporibus
+        necessitatibus assumenda sequi blanditiis.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
