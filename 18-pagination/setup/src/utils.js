7150f1be8e545ad93c1a5d58b97a9ccc8f670ad1
omid <o.beygi@gmail.com>
2022-02-22 18:38:03 +0330

    2/22

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,15 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  //   console.log(followers);
 
-export default paginate
+  const itemPerPage = 10;
+  const pages = Math.ceil(followers.length / itemPerPage);
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemPerPage;
+    return followers.slice(start, start + itemPerPage);
+  });
+
+  return newFollowers;
+};
+
+export default paginate;
